2b007309032b2430da150beb6cbc5f967eac5ae9
machenjie <309272985@qq.com>
2022-06-21 12:02:10 +0800

    test:add test case

--- transforms/__testfixtures__/useHistory_to_useNavigate/import_namespace_specifier.output.tsx
+++ transforms/__testfixtures__/useHistory_to_useNavigate/import_namespace_specifier.output.tsx
@@ -1,25 +1,38 @@
-import * as Router from "react-router-dom";
-import React from "react";
+import * as Router from "react-router-dom"
+import React from "react"
 
-//fixme 间接引用暂不支持，请手动处理
+//fixme don't support
 let c = Router;
 
 function App() {
-  const navigate = Router.useNavigate();
+  const navigate = Router.useNavigate()
+
+  const goForwardFn = React.useCallback(() => {
+    navigate(1)
+  }, [navigate])
+
+  const pushFn = React.useCallback(() => {
+    navigate("/anme1", {
+      state: { id: 13 }
+    })
+  }, [navigate])
+
   return <>
     <button onClick={() => navigate("name", {
       replace: true,
       state: { id: 123 }
     })}>replace</button>
-    <button onClick={//fixme 需手动binding
+    <button onClick={//fixme binding by yourself
     goBack}>Go back</button>
-    <button onClick={//fixme 需手动binding
+    <button onClick={//fixme binding by yourself
     goForward}>Go forward</button>
+    <button onClick={goForwardFn}>Go forward useCallback</button>
     <button onClick={() => navigate(1)}>Go forward</button>
     <button onClick={() => navigate("/anme1")}>push</button>
     <button onClick={() => navigate("/anme1", {
       state: { id: 13 }
     })}>push state</button>
+    <button onClick={pushFn}>push state useCallback</button>
     <button onClick={() => navigate(-2)}>Go -2 pages</button>
   </>;
 }
--- transforms/__testfixtures__/useHistory_to_useNavigate/import_specifier.output.tsx
+++ transforms/__testfixtures__/useHistory_to_useNavigate/import_specifier.output.tsx
@@ -3,20 +3,33 @@ import React from "react";
 
 function App() {
   const navigate = useNavigate();
+
+  const goForwardFn = React.useCallback(() => {
+    navigate(1)
+  }, [navigate])
+
+  const pushFn = React.useCallback(() => {
+    navigate("/user", {
+      state: { id: 13 }
+    })
+  }, [navigate])
+
   return <>
     <button onClick={() => navigate("name", {
       replace: true,
       state: { id: 123 }
     })}>replace</button>
-    <button onClick={//fixme 需手动binding
+    <button onClick={//fixme binding by yourself
     goBack}>Go back</button>
-    <button onClick={//fixme 需手动binding
+    <button onClick={//fixme binding by yourself
     goForward}>Go forward</button>
+    <button onClick={goForwardFn}>Go forward useCallback</button>
     <button onClick={() => navigate(1)}>Go forward</button>
-    <button onClick={() => navigate("/anme1")}>push</button>
-    <button onClick={() => navigate("/anme1", {
+    <button onClick={() => navigate("/user1")}>push</button>
+    <button onClick={() => navigate("/user2", {
       state: { id: 13 }
     })}>push state</button>
+    <button onClick={pushFn}>push state useCallback</button>
     <button onClick={() => navigate(-2)}>Go -2 pages</button>
   </>;
 }
